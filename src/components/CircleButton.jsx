59e6560b9d15d7d367e68cb85e173339055b59ba
yosi <yama.goo.yama.goo@gmail.com>
2023-03-22 20:56:32 +0900

    create components

---
+++ src/components/CircleButton.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { string } from 'prop-types';
+
+export default function CircleButtom(props) {
+  const { children } = props;
+  return (
+    <View style={styles.circleButton}>
+      <Text style={styles.circleButtonLabel}>{children}</Text>
+    </View>
+  );
+}
+
+/* サークルボタンを使用するときは必ずpropsを渡すように設定 isRequiredは必須の項目 初期値を設定する際はdefaultprops */
+CircleButtom.propTypes = {
+  children: string.isRequired,
+};
+
+const styles = StyleSheet.create({
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 40,
+  },
+});
